0f49c25591792b795e8624a89addd45be88a07f8
serch3 <alwovive@gmail.com>
2025-09-16 00:53:31 -0500

    integrate HeroUI and Tailwind CSS into the project

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { HeroUIProvider } from "@heroui/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>
 )
